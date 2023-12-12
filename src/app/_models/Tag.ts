72846daeb707fb9a094903bcf3f9880a2c7e7550
Arnon Nascimento <88281038+4rNoNy@users.noreply.github.com>
2023-12-12 00:33:32 -0300

    Ajustes visuais

--- src/app/_models/Tag.ts
+++ src/app/_models/Tag.ts
@@ -1,14 +1,14 @@
 export class Tag {
-    static readonly ANGULAR = new Tag('Angular', 'red');
-    static readonly CSHARP = new Tag('C#', 'blue');
-    static readonly REACT = new Tag('React', 'red');
-    static readonly TYPESCRIPT = new Tag('TypeScript', 'red');
-    static readonly PYTON = new Tag('Pyton', 'red');
-    static readonly DART = new Tag('Dart', 'red');
-    static readonly ASPNET = new Tag('ASP.NET', 'red');
-    static readonly NODEJS = new Tag('Node.JS', 'red');
-    static readonly FLUTTER = new Tag('Flutter', 'red');
-    private constructor(private readonly key: string, public readonly color: string) {
+    static readonly ANGULAR = new Tag('Angular', 'red','../../assets/linkedin.png');
+    static readonly CSHARP = new Tag('C#', 'blue','../../assets/linkedin.png');
+    static readonly REACT = new Tag('React', 'red','../../assets/linkedin.png');
+    static readonly TYPESCRIPT = new Tag('TypeScript', 'red','../../assets/linkedin.png');
+    static readonly PYTON = new Tag('Pyton', 'red','../../assets/linkedin.png');
+    static readonly DART = new Tag('Dart', 'red','../../assets/linkedin.png');
+    static readonly ASPNET = new Tag('ASP.NET', 'red','../../assets/linkedin.png');
+    static readonly NODEJS = new Tag('Node.JS', 'red','../../assets/linkedin.png');
+    static readonly FLUTTER = new Tag('Flutter', 'red','../../assets/linkedin.png');
+    private constructor(private readonly key: string, public readonly color: string,public readonly iconPath: string) {
 
     }
 
